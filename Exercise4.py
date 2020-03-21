913db9fd1f045c7ad1a765ee17de9263da009fac
tonkkhao1234567890 <62233044+tonkkhao1234567890@users.noreply.github.com>
2020-03-21 15:58:26 +0700

    Create Exercise4.py

---
+++ Exercise4.py
@@ -0,0 +1,10 @@
+#Exercise4 :D
+Foundation_English               = 80
+General_Business                 = 70
+Introduction_to_Computer_Systems = 81
+Computer_Programming             = 90
+print("---Your Score---")
+print("Foundation English               :", Foundation_English)
+print("General Business                 :", General_Business)
+print("Introduction to Computer Systems :",Introduction_to_Computer_Systems)
+print("Computer Programming             :",Computer_Programming )
