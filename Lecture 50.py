d4b39c2db4465aef7ac2dc2df5d1bcbfbcd7ad15
tonkkhao1234567890 <62233044+tonkkhao1234567890@users.noreply.github.com>
2020-04-01 09:44:12 +0700

    Create Lecture 50.py

---
+++ Lecture 50.py
@@ -0,0 +1,12 @@
+def บวกNumber(x,y):
+    print(x+y)
+บวกNumber(100,25)
+def ลบNumber(x,y):
+    print(x-y)
+ลบNumber(100,25)
+def หารNumeber(x,y):
+    print(x/y)
+หารNumeber(100,25)
+def คูณNumber(x,y):
+    print(x*y)
+คูณNumber(100,25)
